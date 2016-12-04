94005240f13d939a61ced2a34493844fe0fc65ec
kachurina <l.kachurina@campus.lmu.de>
2016-12-04 01:09:22 +0100

    Start method in Controller

--- src/Controller.java
+++ src/Controller.java
@@ -1,6 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import views.Pane;
+
 /**
  * Created by gracenote on 04-Dec-16.
  */
-public class Controller {
-    
+public class Controller extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 500, 500);
+        primaryStage = new Stage();
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
